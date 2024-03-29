d172b5daf8f0b1084b7b42d55c71680220b7a917
George <qooisback@yahoo.com>
2024-03-29 13:09:21 -0600

    Moved Login function to own screen.js

--- EventPlanner/App.js
+++ EventPlanner/App.js
@@ -5,27 +5,10 @@ import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import CalendarScreen from "./screens/CalendarScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import Login from "./screens/Login";
 const Stack = createNativeStackNavigator();
 import Search from "./SearchBar";
 import { useNavigation } from "@react-navigation/native";
-
-function Login() {
-  const navigation = useNavigation();
-  return (
-    <View style={styles.container}>
-      <View style={styles.form}>
-        <Text style={styles.label}>Username</Text>
-        <TextInput style={styles.input} placeholder="Enter username" />
-        <Text style={styles.label}>Password</Text>
-        <TextInput style={styles.input} placeholder="Enter password" secureTextEntry/>
-        <View style= {{flexDirection: "row", marginLeft: 20, justifyContent: "space-evenly"}}>
-          <Button title="Login" onPress={() => navigation.navigate("HomeScreen")} />
-          <Button title="Sign Up" onPress={() => navigation.navigate("HomeScreen")} />
-        </View>
-      </View>
-    </View>
-  );
-}
 
 export default function App() {   
 
@@ -49,34 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#fff",
+    alignItems: "center",
     justifyContent: "center",
-    paddingHorizonal: 20,
   },
-  form: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5, 
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 5,
-    fontWeight: "bold",
-  },
-  input: {
-    height: 40,
-    borderColor: "#ddd",
-    borderWidth: 1,
-    marginBottom: 15,
-    padding: 10,
-    borderRadius: 5,
-  }
 });
