2d491c6763342208d7640ff07ed160c85ee6f332
lukeleimbach <luke.leimbach@gmail.com>
2024-03-29 10:57:47 -0600

    Created a theme file with our color palette. Updated home screen and bottom nav bar to reflect theme. Bottom nav bar now uses vector assets instead of png images.

---
+++ EventPlanner/constants/theme.js
@@ -0,0 +1,36 @@
+// All colors in theme
+export const colors = {
+  // Base colors
+  maroon: '#70002e',
+  dark: '#191919',
+  medium: '#454343',
+  light: '#9c9a9a',
+  black: '#000000',
+  white: '#ffffff',
+
+  // Transition Colors (see UM website for actual transition)
+  green: '#1d3c34',
+  tan: '#f0e7d6',
+  orange: '#f94139',
+  blue: '#bcdee8',
+
+  // Interaction Colors
+  success: '#00DE83',
+  warning: '#CADE00',
+  danger: '#ff4b51',
+};
+
+// Home page transition colors
+export const transition_palette_1 = {
+  c1: colors.green,
+  c2: colors.tan,
+  c3: colors.orange,
+};
+
+// Secondary transition colors
+export const transition_palette_2 = {
+  c1: colors.tan,
+  c2: colors.blue,
+  c3: colors.maroon,
+  c4: colors.orange,
+};
